fc2176e60d5fb26f70e232cd1f69a5598e731014
Beckylee <dr.oreo@gmail.com>
2015-11-04 16:35:09 -0500

    created incident form directive, not functional yet

---
+++ browser/js/common/directives/incident-form/incidentForm.js
@@ -0,0 +1,9 @@
+app.directive('incidentForm', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/incident-form/incident-form.html',
+        link: (scope, elem, attr) => {
+
+        }
+    };
+});
