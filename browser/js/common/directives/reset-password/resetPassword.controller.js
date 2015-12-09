df13a7329279122e0808d2e92bd2e50b33af7c01
Beckylee <dr.oreo@gmail.com>
2015-12-09 14:19:14 -0500

    password reset works

--- browser/js/common/directives/reset-password/resetPassword.controller.js
+++ browser/js/common/directives/reset-password/resetPassword.controller.js
@@ -10,10 +10,8 @@ app.controller('passwordCtrl', function($scope, $uibModalInstance, UserFactory, 
     $scope.password= null;
 
     $scope.savePassword= () => {
-      console.log($scope.password, $scope.user);
       $scope.user.something = $scope.password;
-      console.log($scope.user);
-      UserFactory.save($scope.user)
+      UserFactory.update($scope.user._id, {password: $scope.password})
       .then(saved => {
         console.log('success', saved);
         $scope.close();
